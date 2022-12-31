1925770d4dde05ad54b221efb03b68ed0d7fc932
Sangtriever <dltkdgus1850@gmail.com>
2022-12-31 10:45:47 +0900

    FEAT : UserRepository findByNickname 추가 #12

--- src/main/java/com/pulbatte/pulbatte/user/repository/UserRepository.java
+++ src/main/java/com/pulbatte/pulbatte/user/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean findAllByUserId(String userId);
 
     boolean existsByUserId(String UserId);
+
+    boolean findByNickname(String nickname);
 }
