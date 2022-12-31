d82b5be09b2597efc2fea7707a28b557abf52252
Sangtriever <dltkdgus1850@gmail.com>
2022-12-31 11:50:25 +0900

    FEAT : 마이페이지 닉네임 중복확인,수정 , 프로필 이미지 변경, 회원 탈퇴 기능 추가 #12

---
+++ src/main/java/com/pulbatte/pulbatte/mypage/dto/StringDto.java
@@ -0,0 +1,10 @@
+package com.pulbatte.pulbatte.mypage.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class StringDto {
+    private String nickname;
+}
