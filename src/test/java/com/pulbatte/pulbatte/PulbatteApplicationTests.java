e2e34821d873f6b22623fad2c39131bef410e88c
in-gwang <dlsrhkd615@gmail.com>
2023-01-05 17:49:04 +0900

    Fix : 테스트 파일 수정

--- src/test/java/com/pulbatte/pulbatte/PulbatteApplicationTests.java
+++ src/test/java/com/pulbatte/pulbatte/PulbatteApplicationTests.java
@@ -3,7 +3,6 @@ package com.pulbatte.pulbatte;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PulbatteApplicationTests {
 
     @Test
