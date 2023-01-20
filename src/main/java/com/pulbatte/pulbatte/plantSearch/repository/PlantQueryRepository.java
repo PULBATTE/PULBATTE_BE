eea3e35a5c34169c3121737808d2c6227a2c0941
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-01-21 01:31:21 +0900

    REFACTOR: Full Text Search 적용 #87

--- src/main/java/com/pulbatte/pulbatte/plantSearch/repository/PlantQueryRepository.java
+++ src/main/java/com/pulbatte/pulbatte/plantSearch/repository/PlantQueryRepository.java
@@ -1,11 +1,11 @@
 package com.pulbatte.pulbatte.plantSearch.repository;
 
-import com.pulbatte.pulbatte.plantSearch.dto.PlantDetailDto;
-import com.pulbatte.pulbatte.plantSearch.dto.QPlantDetailDto;
+import com.pulbatte.pulbatte.plantSearch.dto.*;
 import com.pulbatte.pulbatte.plantSearch.entity.PlantTag;
-import com.pulbatte.pulbatte.plantSearch.dto.PlantListDto;
-import com.pulbatte.pulbatte.plantSearch.dto.QPlantListDto;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,7 +19,6 @@ import static com.pulbatte.pulbatte.plantSearch.entity.QPlant.plant;
 @Repository
 public class PlantQueryRepository {
     private final JPAQueryFactory queryFactory;
-
     public PlantQueryRepository(JPAQueryFactory queryFactory) {
         this.queryFactory = queryFactory;
     }
@@ -47,14 +46,24 @@ public class PlantQueryRepository {
     }
 
     // 식물 이름 검색
+//    public List<PlantListDto> findByPlantName(@Param("plantName") String plantName) {
+//        return queryFactory
+//                .select(new QPlantListDto(
+//                        plant
+//                ))
+//                .from(plant)
+//                .where(eqPlantName(plantName))
+//                .orderBy(plant.plantName.asc())
+//                .fetch();
+//    }
+
     public List<PlantListDto> findByPlantName(@Param("plantName") String plantName) {
         return queryFactory
                 .select(new QPlantListDto(
-                        plant
+                plant
                 ))
                 .from(plant)
                 .where(eqPlantName(plantName))
-                .orderBy(plant.plantName.asc())
                 .fetch();
     }
 
@@ -70,11 +79,21 @@ public class PlantQueryRepository {
                 .fetch();
     }
 
+//    private BooleanExpression eqPlantName(String plantName) {
+//        if(ObjectUtils.isEmpty(plantName)) {
+//            return null;
+//        }
+//        return plant.plantName.contains(plantName);
+//    }
+
     private BooleanExpression eqPlantName(String plantName) {
         if(ObjectUtils.isEmpty(plantName)) {
             return null;
         }
-        return plant.plantName.contains(plantName);
+        NumberTemplate booleanTemplate = Expressions.numberTemplate(
+                Double.class,
+                "function('match',{0},{1})", plant.plantName, plantName);
+        return booleanTemplate.gt(0);
     }
 
     private BooleanExpression eqPlantTag(PlantTag tag) {
