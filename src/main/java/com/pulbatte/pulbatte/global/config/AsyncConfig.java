2537f666a5cfc1410e0e4d4c9392e284ef0a12b5
syp0812 <94878158+syp0812@users.noreply.github.com>
2023-02-04 21:10:26 +0900

    FEAT: AsyncUncaughtExceptionHandler 추가 #116

--- src/main/java/com/pulbatte/pulbatte/global/config/AsyncConfig.java
+++ src/main/java/com/pulbatte/pulbatte/global/config/AsyncConfig.java
@@ -1,6 +1,8 @@
 package com.pulbatte.pulbatte.global.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -8,8 +10,9 @@ import java.util.concurrent.Executor;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig extends AsyncConfigurerSupport {
 
+    @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setCorePoolSize(5);
@@ -18,5 +21,10 @@ public class AsyncConfig {
         threadPoolTaskExecutor.setThreadNamePrefix("async-thread-");
         threadPoolTaskExecutor.initialize();
         return threadPoolTaskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return super.getAsyncUncaughtExceptionHandler();
     }
 }
