96f22b120ed049a6409aae6a38aa17ea187e522f
in-gwnag <117730606+ingwang-kim@users.noreply.github.com>
2023-02-10 01:12:03 +0900

    FIX : refreshToken

--- src/main/java/com/pulbatte/pulbatte/global/service/TokenService.java
+++ src/main/java/com/pulbatte/pulbatte/global/service/TokenService.java
@@ -25,7 +25,7 @@ public class TokenService {
 
     // 토큰 재발행
     @Transactional
-    public TokenDto reFreshToken(/*User user,*//*String authorization,*/HttpServletResponse response, RequestToken requestToken){
+    public TokenDto reFreshToken(/*User user,*/String authorization,HttpServletResponse response, RequestToken requestToken){
         TokenDto tokenDto;
         User user = userRepository.findByUserId(requestToken.getUserEmail()).orElseThrow(
                 () -> new CustomException(ErrorCode.NO_EXIST_USER)
@@ -33,7 +33,7 @@ public class TokenService {
         RefreshToken refreshToken = refreshTokenRepository.findByAccountUserId(user.getUserId()).orElseThrow(
                 () -> new CustomException(ErrorCode.DISMATCH_TOKEN)
         );
-        if(refreshToken.getRefreshToken().equals(requestToken.getRefreshToken())/*&&refreshToken.getAccessToken().equals(authorization)*/){
+        if(refreshToken.getRefreshToken().equals(requestToken.getRefreshToken())&&refreshToken.getAccessToken().equals(authorization)){
             response.addHeader(JwtUtil.ACCESS_TOKEN, jwtUtil.createToken(user.getUserId()));
             tokenDto = jwtUtil.createAllToken(user.getUserId());
             refreshToken.updateToken(tokenDto.getRefreshToken(),tokenDto.getAccessToken());
